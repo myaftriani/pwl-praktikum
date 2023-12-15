91d51d37d3b04e176704c437ba6fddcc45fac746
rimamustika <rimamustika2003@gmail.com>
2023-12-16 00:22:01 +0700

    menambahkan view index books

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,6 +17,12 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('book')" :active="request()->routeIs('book')">
+                        {{ __('Book') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
